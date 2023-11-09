63bad4cea84b487d4212b9ec437f5efc01839c9b
EgorVereshko <72032005+EgorVereshko@users.noreply.github.com>
2023-11-09 15:05:59 +0500

    Update task5.ts

--- Practice2/task5.ts
+++ Practice2/task5.ts
@@ -1,24 +1,11 @@
-function areEqual<T extends object>(obj_1:T, obj_2:T): boolean {
-    const keys_a = Object.keys(obj_1);
-    const keys_b = Object.keys(obj_2);
-
-    if (keys_a.length != keys_b.length) {
-        return false;
-    }
-
-    for (const key of keys_a) {
-        if (!keys_b.includes(key) || obj_1[key] != obj_2[key]) {
-            return false;
-        }
-    }
-
-    return true;
+function areEqual<T extends object>(obj1: T, obj2: T): boolean {
+    return JSON.stringify(obj1) == JSON.stringify(obj2)
 }
 
 const obj_1 = { a: 1, b: 2, c: 3 };
 const obj_2 = { a: 1, b: 2, c: 3 };
 console.log(areEqual(obj_1, obj_2)); // true
 
-// const obj_1 = { a: 1, b: 2, c: 3 };
-// const obj_2 = { a: 1, b: 2, c: 4 };
-// console.log(areEqual(obj_1, obj_2)); // false
+const obj_3 = { a: 1, b: 2, c: 3 };
+const obj_4 = { a: 1, b: 2, c: 4 };
+console.log(areEqual(obj_3, obj_4)); // false
